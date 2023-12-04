b39b55d959c5030506b87480bc1676840bc8f82d
Stanimir Dimitrov <evlution63@gmail.com>
2023-12-04 17:59:27 +0200

    refactor: remove unused imports

--- src/main/java/com/cscb525/project/service/TransportCompanyService.java
+++ src/main/java/com/cscb525/project/service/TransportCompanyService.java
@@ -2,7 +2,6 @@ package com.cscb525.project.service;
 
 import com.cscb525.project.dto.TransportCompanyDto;
 import com.cscb525.project.dto.TransportCompanyDtoResponse;
-import com.cscb525.project.model.TransportCompany;
 
 import java.util.List;
 
--- src/main/java/com/cscb525/project/service/implementation/TransportCompanyServiceImpl.java
+++ src/main/java/com/cscb525/project/service/implementation/TransportCompanyServiceImpl.java
@@ -8,12 +8,10 @@ import com.cscb525.project.service.TransportCompanyService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
