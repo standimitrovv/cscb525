f35c90dcf105b2910fba8b70614ebad34ccaa0a3
Stanimir Dimitrov <evlution63@gmail.com>
2023-12-11 13:23:34 +0200

    feat: add an Employee dto and response

---
+++ src/main/java/com/cscb525/project/dto/EmployeeDto.java
@@ -0,0 +1,23 @@
+package com.cscb525.project.dto;
+
+import com.cscb525.project.model.DrivingQualification;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeDto {
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private DrivingQualification drivingQualification;
+
+    @NotBlank
+    private double salary;
+}
