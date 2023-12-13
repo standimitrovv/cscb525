a2ace78a9ef1027290fd470d2c820ba646a1557f
Stanimir Dimitrov <evlution63@gmail.com>
2023-12-13 10:51:20 +0200

    feat: create a class for exception messages

---
+++ src/main/java/com/cscb525/project/exception/ExceptionTextMessages.java
@@ -0,0 +1,8 @@
+package com.cscb525.project.exception;
+
+public class ExceptionTextMessages {
+    public static final String COMPANY_VEHICLE_NOT_FOUND = "Company doesn't have an assigned vehicle with the provided id!";
+    public static final String COMPANY_CLIENT_NOT_FOUND = "Company doesn't have an assigned client with the provided id!";
+    public  static final String COMPANY_EMPLOYEE_NOT_FOUND = "Company doesn't have an assigned employee with the provided id!";
+    public static final String CARGO_WEIGHT_NOT_FOUND = "Cargo weight is required but not defined";
+}
